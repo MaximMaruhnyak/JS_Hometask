23c212dd15f9068fc48d2683796f2f345f401f28
Maxim Maruhnyak <mmaruhnyak@Maxims-MacBook-Pro.local>
2016-10-25 00:31:49 +0300

    second part of first task

--- callback_2.js
+++ callback_2.js
@@ -2,3 +2,34 @@
  * Created by mmaruhnyak on 10/24/16.
  */
 
+var popsicle = require('popsicle')
+
+function getResponse(title, due, onSuccess, onFailure) {
+  popsicle.request({
+      method: 'POST',
+      url: 'http://lacedeamon.spartaglobal.com/todos',
+      body: {
+        title: title,
+        due: due
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    .then(function (res) {
+      if (res.status == 201)
+        onSuccess('User was created succesfully')
+      else {
+        onFailure("Can not create user")
+      }
+    })
+}
+
+getResponse('Maximus', 15,
+  function (result) {
+    console.log(result)
+  },
+  function (result) {
+    console.log(result)
+  }
+);
